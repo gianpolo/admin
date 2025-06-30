fbb1727aa8d886893f55956e8a40e202cbf8ca24
Gianpiero Ferraro <gianpiero.ferraro@citywonders.com>
2025-07-01 01:04:22 +0200

    handling ConfigurationSlotsGeneratedIntegrationEvent

--- src/components/notifications/NotificationsWidget.jsx
+++ src/components/notifications/NotificationsWidget.jsx
@@ -8,6 +8,7 @@ import { fetchEventsLogs } from "../../store/notificationsSlice.js";
 export default function NotificationsWidget() {
   const dispatch = useDispatch();
   const notifications = useSelector((state) => state.notifications.items);
+  const logs = useSelector((state) => state.notifications.logs);
   const { id } = useParams();
   useEffect(() => {
     dispatch(fetchEventsLogs(id));
@@ -29,40 +30,63 @@ export default function NotificationsWidget() {
 
   return (
     <ComponentCard title="Events log">
-      {notifications.length === 0 ? (
-        <p className=" text-sm text-gray-500 dark:text-gray-400">
-          No notifications
-        </p>
+      {logs.length === 0 && notifications.length === 0 ? (
+        <p className=" text-sm text-gray-500 dark:text-gray-400">No history</p>
       ) : (
         <>
           <div className="max-h-64 overflow-y-auto custom-scrollbar">
             <Table>
               <TableBody>
+                {logs.map((n, idx) => {
+                  return (
+                    <TableRow className="text-[10px]" key={n.createdOn}>
+                      <TableCell className="px-2 py-2 sm:px-2 text-start ">
+                        <div className="flex items-center gap-3">
+                          <span
+                            className={`mr-2 rounded-full h-8 w-8  flex items-center justify-center text-white ${
+                              eventsData[n.context][n.eventName].bg
+                            }`}
+                          >
+                            {eventsData[n.context][n.eventName].icon}
+                          </span>
+                          <div className="text-[10px]">
+                            <span className="block font-medium text-gray-800 text-theme-sm dark:text-white/90">
+                              {eventsData[n.context][n.eventName].text}
+                            </span>
+                            <span className="block text-gray-500 text-theme-xs dark:text-gray-400">
+                              {new Date(n.createdOn).toLocaleString()}
+                            </span>
+                          </div>
+                        </div>
+                      </TableCell>
+                    </TableRow>
+                  );
+                })}
                 {notifications.map((n, idx) => {
                   return (
                     <TableRow className="text-[10px]" key={n.createdOn}>
                       <TableCell className="px-2 py-2 sm:px-2 text-start ">
                         <div className="flex items-center gap-3">
-                        <span
-                          className={`mr-2 rounded-full h-8 w-8  flex items-center justify-center text-white ${
-                            eventsData[n.context][n.eventName].bg
-                          }`}
-                        >
-                          {eventsData[n.context][n.eventName].icon}
-                        </span>
-                        <div className="text-[10px]">
-                          <span className="block font-medium text-gray-800 text-theme-sm dark:text-white/90">
-                            {eventsData[n.context][n.eventName].text}
+                          <span
+                            className={`mr-2 rounded-full h-8 w-8  flex items-center justify-center text-white ${
+                              eventsData[n.context][n.eventName].bg
+                            }`}
+                          >
+                            {eventsData[n.context][n.eventName].icon}
                           </span>
-                          <span className="block text-gray-500 text-theme-xs dark:text-gray-400">
-                            {new Date(n.createdOn).toLocaleString()}
-                          </span>
+                          <div className="text-[10px]">
+                            <span className="block font-medium text-gray-800 text-theme-sm dark:text-white/90">
+                              {eventsData[n.context][n.eventName].text}
+                            </span>
+                            <span className="block text-gray-500 text-theme-xs dark:text-gray-400">
+                              {new Date(n.createdOn).toLocaleString()}
+                            </span>
+                          </div>
                         </div>
-                      </div>
-                    </TableCell>
-                  </TableRow>
-                );
-              })}
+                      </TableCell>
+                    </TableRow>
+                  );
+                })}
               </TableBody>
             </Table>
           </div>
